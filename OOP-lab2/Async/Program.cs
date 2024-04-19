f05679c5f75c916be004a16cdf4ae8be4c854388
CTaPuH4 <ilya.nizhellskiy@gmail.com>
2024-04-19 19:19:08 +0300

    1-2

---
+++ OOP-lab2/Async/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        string api_link = "https://api.hh.ru/openapi/redoc#section/Obshaya-informaciya/Vybor-s";
+
+        await Request(api_link);
+        await Request(api_link);
+        await Request(api_link);
+
+        Console.WriteLine("Done!");
+    }
+
+    public static async Task Request(string a)
+    {
+
+        try
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(a);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Response from {a}: {responseBody}");
+                }
+                else
+                {
+                    Console.WriteLine($"Fail. Status code: {response.StatusCode}");
+                }
+            }
+        }
+
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    }
---
+++ OOP-lab2/Sync/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        string api_link = "https://api.hh.ru/openapi/redoc#section/Obshaya-informaciya/Vybor-s";
+        
+        string response = Request(api_link);
+
+        Console.WriteLine(response);
+        Console.WriteLine(response);
+        Console.WriteLine(response);
+
+        Console.WriteLine("Done!");
+    }
+
+    public static string Request(string a)
+    {
+        try
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(a).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return response.Content.ReadAsStringAsync().Result;
+                }
+                else
+                {
+                    return $"Fail. Status code: {response.StatusCode}";
+                }
+            }
+        }
+        catch (HttpRequestException ex)
+        {
+            return $"Error: {ex.Message}";
+        }
+    }
+}
